fa891cbc7065df266edb1e348d0cbcdf17493d0d
yi. <2304816231@qq.com>
2026-05-17 17:12:48 +0800

    remove retired front assets

--- flyflor-front/server/api/auth/logout.post.ts
+++
@@ -1,9 +0,0 @@
-import { clearAuthSession } from "../../utils/auth";
-
-export default defineEventHandler((event) => {
-    clearAuthSession(event);
-
-    return {
-        ok: true,
-    };
-});
--- flyflor-front/server/api/auth/me.get.ts
+++
@@ -1,7 +0,0 @@
-import { getCurrentUser } from "../../utils/auth";
-
-export default defineEventHandler((event) => {
-    return {
-        user: getCurrentUser(event),
-    };
-});
